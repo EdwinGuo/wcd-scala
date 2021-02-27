a532b7584b1348cab8dc31466bd3458ccd85a382
edwinguo <altergzj@yahoo.com>
2021-02-27 09:43:38 -0500

    Tring to make a comment

--- src/main/scala/com/weclouddata/Server.scala
+++ src/main/scala/com/weclouddata/Server.scala
@@ -8,6 +8,7 @@ import com.weclouddata.exercises.Exercises
 object Server extends Context {
   val route: Route = Exercises.route
 
+  // this is a comment
   def main(args: Array[String]): Unit = {
     Http().bindAndHandle(route, Settings.Http.host, Settings.Http.port)
   }
